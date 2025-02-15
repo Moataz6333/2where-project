2a72ad77789e15709bb37f9679bd6ea2f276b37f
Moataz Ahmed <moatazahmedghander2003@gmail.com>
2025-02-15 14:52:20 +0200

    rest and hotels updateds

--- app/Http/Controllers/Api/HotelController.php
+++ app/Http/Controllers/Api/HotelController.php
@@ -33,6 +33,100 @@ class HotelController extends Controller
                 'errors' => $validator->errors(),  // Return validation errors
             ], 422);  // 422 Unprocessable Entity
         }
+        
+        $hotel = Hotel::where('user_id',auth()->user()->id)->first();
+        if($hotel){
+            $hotel->title = $request->title;
+        $hotel->rate = (int) $request->rate;
+        $hotel->features = $request->features;
+        $hotel->price = $request->price;
+        $hotel->address = $request->address;
+        $hotel->link=$request->link;
+        $hotel->save();
+        if ($request->hasFile('postPhoto')) {
+
+            $photo=Photo::where('hotel_id',$hotel->id)->where('type','post')->first(); 
+            $file = $_SERVER['DOCUMENT_ROOT'].$photo->path;
+            unlink($file);
+
+            $mainPhoto = $request->file('postPhoto');
+            $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/images/userHotels';
+            if (!file_exists($destinationPath)) {
+                mkdir($destinationPath, 0777, true);
+            }
+            $fileName =  $mainPhoto->getClientOriginalName();
+
+            $mainPhoto->move($destinationPath, $fileName);
+            $relativePath = '/images/userHotels/' . $fileName; 
+          
+            $photo->path=$relativePath;
+            $photo->type = "post";
+            $photo->save();
+         
+        }
+        if ($request->hasFile('images')) {
+            // del the old photos
+            $oldimages =Photo::where('hotel_id',$hotel->id)->where('type','slider')->get();
+            foreach($oldimages as $oldImage){
+                $file = $_SERVER['DOCUMENT_ROOT'].$oldImage->path;
+                unlink($file);
+                $oldImage->delete();
+            }
+              
+            $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/images/userHotels/'.$hotel->id;
+            if (!file_exists($destinationPath)) {
+                mkdir($destinationPath, 0777, true);
+            }
+            foreach ($request->file('images') as $mainPhoto) {
+               
+                $fileName =  $mainPhoto->getClientOriginalName();
+                
+                // Move the file to the destination path
+                $mainPhoto->move($destinationPath, $fileName);
+                $relativePath = '/images/userHotels/'.$hotel->id.'/' . $fileName;
+                $photo = new Photo();
+                $photo->hotel_id=$hotel->id;
+                $photo->path= $relativePath;
+                $photo->type='slider';
+                $photo->save();
+                
+            }
+       }
+        if ($request->hasFile('proofs')) {
+            // del
+               // del the old photos
+               $oldimages =Photo::where('hotel_id',$hotel->id)->where('type','proofs')->get();
+               foreach($oldimages as $oldImage){
+                   $file = $_SERVER['DOCUMENT_ROOT'].$oldImage->path;
+                   unlink($file);
+                   $oldImage->delete();
+               }
+              
+            $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/images/userHotels/'.$hotel->id;
+            if (!file_exists($destinationPath)) {
+                mkdir($destinationPath, 0777, true);
+            }
+            foreach ($request->file('proofs') as $mainPhoto) {
+               
+                $fileName =  $mainPhoto->getClientOriginalName();
+                
+                // Move the file to the destination path
+                $mainPhoto->move($destinationPath, $fileName);
+                $relativePath = '/images/userHotels/'.$hotel->id.'/' . $fileName;
+                $photo = new Photo();
+                $photo->hotel_id=$hotel->id;
+                $photo->path= $relativePath;
+                $photo->type='proofs';
+                $photo->save();
+                
+            }
+            return response()->json(['hotel'=>$hotel,
+            "message"=>"hotel updated successfully"],200);
+       }
+
+        }else{
+
+       
         $hotel =new Hotel();
 
         $hotel->city_id=1;
@@ -125,6 +219,7 @@ class HotelController extends Controller
 
 
         
-        return response()->json($hotel,201,['message'=>'Hotel requested successfully!']);
+        return response()->json(['hotel'=> $hotel,'message'=>'Hotel requested successfully!'],201);
+    }
     }
 }
--- app/Http/Controllers/Api/RestaruantController.php
+++ app/Http/Controllers/Api/RestaruantController.php
@@ -34,6 +34,132 @@ class RestaruantController extends Controller
                 'errors' => $validator->errors(),  // Return validation errors
             ], 422);  // 422 Unprocessable Entity
         }
+        // first or create
+        $rest = Restaruant::where('user_id',auth()->user()->id)->first();
+        if($rest){
+            // update
+            $rest->title = $request->title;
+            $rest->rate = (int) $request->rate;
+            $rest->categories = $request->categories;
+            $rest->price = $request->price;
+            $rest->address = $request->address;
+            $rest->hours=$request->hours;
+            $rest->save();
+            if ($request->hasFile('postPhoto')) {
+
+                $photo=Photo::where('restaruant_id',$rest->id)->where('type','post')->first(); 
+                $file = $_SERVER['DOCUMENT_ROOT'].$photo->path;
+                unlink($file);
+
+                $mainPhoto = $request->file('postPhoto');
+                $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/images/userRests';
+                if (!file_exists($destinationPath)) {
+                    mkdir($destinationPath, 0777, true);
+                }
+                $fileName =  $mainPhoto->getClientOriginalName();
+    
+                $mainPhoto->move($destinationPath, $fileName);
+                $relativePath = '/images/userRests/' . $fileName; 
+                
+              
+                $photo->path=$relativePath;
+
+                $photo->save();
+           
+            } 
+             if ($request->hasFile('menu')) {
+                // delete the old ones
+                $oldMenu =Photo::where('restaruant_id',$rest->id)->where('type','menu')->get();
+                foreach($oldMenu as $menu){
+                    $file = $_SERVER['DOCUMENT_ROOT'].$menu->path;
+                    unlink($file);
+                    $menu->delete();
+                }
+                  
+                $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/images/userRests/'.$rest->id.'/menus';
+                if (!file_exists($destinationPath)) {
+                    mkdir($destinationPath, 0777, true);
+                }
+                foreach ($request->file('menu') as $mainPhoto) {
+
+                    $fileName =  $mainPhoto->getClientOriginalName();
+                    
+                    // Move the file to the destination path
+                    $mainPhoto->move($destinationPath, $fileName);
+                    $relativePath = '/images/userRests/'.$rest->id.'/menus/' . $fileName;
+                    $photo = new Photo();
+                    $photo->restaruant_id=$rest->id;
+                    $photo->path= $relativePath;
+                    $photo->type='menu';
+                    $photo->save();
+                    
+                }
+           }
+            if ($request->hasFile('images')) {
+                // del the old photos
+                $oldimages =Photo::where('restaruant_id',$rest->id)->where('type','slider')->get();
+                foreach($oldimages as $oldImage){
+                    $file = $_SERVER['DOCUMENT_ROOT'].$oldImage->path;
+                    unlink($file);
+                    $oldImage->delete();
+                }
+                  
+                $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/images/userRests/'.$rest->id;
+                if (!file_exists($destinationPath)) {
+                    mkdir($destinationPath, 0777, true);
+                }
+                foreach ($request->file('images') as $mainPhoto) {
+                   
+                    $fileName =  $mainPhoto->getClientOriginalName();
+                    
+                    // Move the file to the destination path
+                    $mainPhoto->move($destinationPath, $fileName);
+                    $relativePath = '/images/userRests/'.$rest->id.'/' . $fileName;
+                    $photo = new Photo();
+                    $photo->restaruant_id=$rest->id;
+                    $photo->path= $relativePath;
+                    $photo->type='slider';
+                    $photo->save();
+                    
+                }
+           }
+            if ($request->hasFile('proofs')) {
+                // del
+                   // del the old photos
+                   $oldimages =Photo::where('restaruant_id',$rest->id)->where('type','proofs')->get();
+                   foreach($oldimages as $oldImage){
+                       $file = $_SERVER['DOCUMENT_ROOT'].$oldImage->path;
+                       unlink($file);
+                       $oldImage->delete();
+                   }
+                  
+                $destinationPath = $_SERVER['DOCUMENT_ROOT'] . '/images/userRests/'.$rest->id;
+                if (!file_exists($destinationPath)) {
+                    mkdir($destinationPath, 0777, true);
+                }
+                foreach ($request->file('proofs') as $mainPhoto) {
+                   
+                    $fileName =  $mainPhoto->getClientOriginalName();
+                    
+                    // Move the file to the destination path
+                    $mainPhoto->move($destinationPath, $fileName);
+                    $relativePath = '/images/userRests/'.$rest->id.'/' . $fileName;
+                    $photo = new Photo();
+                    $photo->restaruant_id=$rest->id;
+                    $photo->path= $relativePath;
+                    $photo->type='proofs';
+                    $photo->save();
+                    
+                }
+           }
+
+        return response()->json(['rest'=>$rest,
+    "message"=>"resturant updated successfully"],200);
+
+
+        }else{
+            // create
+        
         $rest =new Restaruant();
 
         $rest->city_id=1;
@@ -148,7 +274,9 @@ class RestaruantController extends Controller
 
 
         
-        return response()->json($rest,200,['message'=>'Restaruant requested successfully!']);
+        return response()->json(['rest'=>$rest,
+            'message'=>'Restaruant requested successfully!'],200);
+      }
     }
     
 }
